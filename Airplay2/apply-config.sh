08590aae4b193fe5dcd6c527550e76e04bb0e0ec
Johann <johannvr@gmx.de>
2025-01-27 23:19:35 +0100

    added interpolation option

--- Airplay2/apply-config.sh
+++ Airplay2/apply-config.sh
@@ -48,6 +48,26 @@ escaped_value=$(echo "$value" | sed 's/\\//g' | sed 's/\$/\\\\$/g')
 # Replace line in config file using sed (target 72nd line)
 sed -i "72s/.*/        audio_backend_latency_offset_in_seconds = $escaped_value/" "$config_file"
 
+################################################### interpolation ###################################################
+
+# Get arguments
+json_key="interpolation"
+
+# Extract value from JSON using jq
+value=$(jq -r ".$json_key" "$json_file")
+
+# Check if value extraction was successful
+if [ $? -ne 0 ]; then
+  echo "Error: Could not extract value from JSON using key '$json_key'."
+  exit 1
+fi
+
+# Escape backslashes and dollar signs for safe sed usage
+escaped_value=$(echo "$value" | sed 's/\\//g' | sed 's/\$/\\\\$/g')
+
+# Replace line in config file using sed (target 18th line)
+sed -i "18s/.*/        interpolation =  $escaped_value/" "$config_file"
+
 ################################################### mqtt setting ###################################################
 
 # Get arguments
